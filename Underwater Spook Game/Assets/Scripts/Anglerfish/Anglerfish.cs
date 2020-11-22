00d7161ce5d62fd213a6ad7ad0d95ac065866434
TerraDev64 <57131645+TerraDev64@users.noreply.github.com>
2020-11-22 22:30:41 +0100

    added following player movement

--- Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
+++ Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
@@ -1,16 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.AI;
+public class Anglerfish : MonoBehaviour {
+    
+    public float speed;
+    private Transform target;
+    public int rotationAngle = 90;
 
-public class Anglerfish : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+
+        transform.LookAt(target);
+        if(Vector3.Distance(transform.position, target.position) > 100)
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
