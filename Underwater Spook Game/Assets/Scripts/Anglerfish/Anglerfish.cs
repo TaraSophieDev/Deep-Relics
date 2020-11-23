53a9c64789d0fd3ee056012b4e99eb30d3bfcb33
TerraDev64 <57131645+TerraDev64@users.noreply.github.com>
2020-11-23 18:00:58 +0100

    added funcs for looking and moving

--- Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
+++ Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
@@ -1,21 +1,45 @@
 ﻿using System;
+using System.Numerics;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.AI;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
 public class Anglerfish : MonoBehaviour {
     
     public float speed;
     private Transform target;
-    public int rotationAngle = 90;
+    public float turnSpeed = 0f;
 
     void Start() {
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        
+        transform.Rotate(new Vector3(0, 90, 0));
     }
 
     private void Update() {
 
-        transform.LookAt(target);
-        if(Vector3.Distance(transform.position, target.position) > 100)
+        LookAtPlayer();
+        MoveToPlayer();
+    }
+
+    void LookAtPlayer() {
+        // Determine which direction to rotate towards
+        Vector3 targetDirection = target.position - transform.position;
+
+        // The step size is equal to speed times frame time.
+        float singleStep = turnSpeed * Time.deltaTime / 10;
+
+        // Rotate the forward vector towards the target direction by one step
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+        
+        // Calculate a rotation a step closer to the target and applies rotation to this object
+        transform.rotation = Quaternion.LookRotation(newDirection);
+    }
+
+    void MoveToPlayer() {
+        if(Vector3.Distance(transform.position, target.position) > 50)
             transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
