a1d13b1dc728bc632bb92e87d7990a76083b2423
TerraDev64 <57131645+TerraDev64@users.noreply.github.com>
2020-11-29 23:10:18 +0100

    fixed fish ai movement and collision.

--- Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
+++ Underwater Spook Game/Assets/Scripts/Anglerfish/Anglerfish.cs
@@ -12,27 +12,35 @@ public class Anglerfish : MonoBehaviour {
     public float speed;
     private Transform target;
     public float turnSpeed = 0f;
-    private Rigidbody rb;
-    public float targetDistance; 
+    public Rigidbody rb;
+    public float targetDistance;
+    private Vector3 movement;
 
     void Start() {
+        //rb = this.GetComponent<Rigidbody>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         transform.Rotate(new Vector3(0, 90, 0));
-        rb.isKinematic = true;
+        //rb.isKinematic = true;
+    }
+
+    private void Update() {
+        LookAtTarget();
+        targetDistance = Vector3.Distance(target.position, transform.position);
+        Vector3 direction = target.position - transform.position;
+        direction.Normalize();
+        movement = direction;
     }
 
     private void FixedUpdate() {
-
-        LookAtPlayer();
-        MoveToPlayer();
+        MoveToTarget(movement);
     }
 
-    void LookAtPlayer() {
+    void LookAtTarget() {
         // Determine which direction to rotate towards
         Vector3 targetDirection = target.position - transform.position;
 
         // The step size is equal to speed times frame time.
-        float singleStep = turnSpeed * Time.deltaTime / 10;
+        float singleStep = turnSpeed * Time.fixedDeltaTime / 10;
 
         // Rotate the forward vector towards the target direction by one step
         Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
@@ -41,13 +49,10 @@ public class Anglerfish : MonoBehaviour {
         transform.rotation = Quaternion.LookRotation(newDirection);
     }
 
-    void MoveToPlayer() {
-        targetDistance = Vector3.Distance(target.position, transform.position);
-        Vector3 direction = target.position - transform.position;
-        if (targetDistance > 40f) {
-            transform.position = targetDistance * (speed * Time.deltaTime);
+    void MoveToTarget(Vector3 direction) {
+        rb.velocity = direction * speed;
+        if (targetDistance > 30f) {
+            
         }
-        rb.MovePosition(transform.position - target.position * speed * Time.deltaTime);
-        //transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
     }
 }
