5fe4837cab75cda6428aac45309572290503fe9f
TerraDev64 <57131645+TerraDev64@users.noreply.github.com>
2020-12-09 18:05:37 +0100

    added SubmarineHealth.cs and a health system

---
+++ Deep Relics/Assets/Scripts/Player/SubmarineHealth.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class SubmarineHealth : MonoBehaviour {
+    public float healthValue = 100;
+    //public TMP_Text health;
+    public float decimalHealthValue;
+
+    public float playerHealth;
+    public TMP_Text health;
+
+
+    private void Update() {
+        if (playerHealth <= 0) {
+            Application.Quit();
+        }
+    }
+
+    private void Start() {
+        UpdateHealth();
+    }
+
+    public void UpdateHealth() {
+        health.text = playerHealth.ToString("0");
+        health.text += "%";
+    }
+}
