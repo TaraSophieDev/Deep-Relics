5ccb5b07ee816c3711fa548176d96e1c8f414267
TerraDev64 <57131645+TerraDev64@users.noreply.github.com>
2020-12-09 22:10:42 +0100

    Added winscreencontroller script for closing or restart the game

---
+++ Deep Relics/Assets/Scripts/Other/WinScreenController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.XR.WSA.Input;
+
+public class WinScreenController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        CloseGame();
+        RestartGame();
+    }
+
+    void CloseGame() {
+        if (Input.GetKey(KeyCode.X)) {
+            Application.Quit();
+            print("Quit");
+        }
+    }
+
+    void RestartGame() {
+        if (Input.GetKey(KeyCode.R)) {
+            SceneManager.LoadScene("Scenes/Game");
+            print("restart");
+        }
+    }
+        
+}
